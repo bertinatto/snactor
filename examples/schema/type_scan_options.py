04d6fbac7691f028f595922ecdf48da7bc4028dc
Fabio Bertinatto <fabiojrb@gmail.com>
2017-08-29 15:26:25 +0200

    Fix typo in type_scan_options

--- examples/schema/type_scan_options.py
+++ examples/schema/type_scan_options.py
@@ -2,7 +2,7 @@ from jsl import Document
 from jsl.fields import BooleanField, StringField
 
 
-class TypePortScan(Document):
+class TypeScanOptions(Document):
     shallow_scan = BooleanField(required=False)
     port_range = StringField(required=False)
     force_nmap = BooleanField(required=False)
